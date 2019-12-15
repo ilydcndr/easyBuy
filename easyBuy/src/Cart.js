07852b9207f00d931720aa1522c1d31bde8aa7b4
ilydcndr <ilaydacundar@hotmail.com>
2019-12-15 22:41:51 +0200

    c

---
+++ easyBuy/src/Cart.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+ class Cart extends Component {
+     state= {
+         deger:""
+     }
+    render() {
+        return (
+            <div>
+                Cart içi!czdasda
+            </div>
+        )
+    }
+}
+export default Cart;
