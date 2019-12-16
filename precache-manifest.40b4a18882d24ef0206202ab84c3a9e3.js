e276031f0e48e037a578310b61d42fec1d307ed5
ilydcndr <ilaydacundar@hotmail.com>
2019-12-17 00:25:12 +0200

    Updates

---
+++ precache-manifest.40b4a18882d24ef0206202ab84c3a9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c879ff1bb3c1af23802476d23f20c2a",
+    "url": "/easyBuy/index.html"
+  },
+  {
+    "revision": "a0c8e29a1ca358ab95fe",
+    "url": "/easyBuy/static/css/2.3542c476.chunk.css"
+  },
+  {
+    "revision": "a0c8e29a1ca358ab95fe",
+    "url": "/easyBuy/static/js/2.190ef16f.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/easyBuy/static/js/2.190ef16f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48304f1dffba0b7da3ee",
+    "url": "/easyBuy/static/js/main.bef43b03.chunk.js"
+  },
+  {
+    "revision": "04004678d854b905e5df",
+    "url": "/easyBuy/static/js/runtime-main.a8228a12.js"
+  }
+]);
